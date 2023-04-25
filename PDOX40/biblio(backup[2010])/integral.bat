42a5815a2c50e396f1e34e9f73e0b13fcc67c44f
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-04-25 10:12:43 -0500

    added new version of UCD Archive provided by JS Noyes

---
+++ PDOX40/biblio(backup[2010])/integral.bat
@@ -0,0 +1,99 @@
+f:
+dir
+pause
+cd pdoxdata
+cd hymenopt
+cd chalcids
+c:
+copy hknew.* f:
+copy language.* f:
+copy Tran.* f:
+copy hostfam.db f:
+copy hostfam.f f:
+copy hostfam.f1 f:
+copy hostfam.px f:
+copy hostfam.val f:
+copy hosplant.db f:
+copy newhost.db f:
+copy master.db f:
+copy master.f f:
+copy master.f1 f:
+copy master.f2 f:
+copy master.f3 f:
+copy master.f4 f:
+copy master.f8 f:
+copy master.px f:
+copy master.xg1 f:
+copy master.xg2 f:
+copy master.yg0 f:
+copy master.yg1 f:
+copy master.yg2 f:
+copy famtrib.* f:
+copy genus.* f:
+copy species.* f:
+copy tstat.* f:
+copy hosts.* f:
+copy refs.* f:
+copy dist.* f:
+copy refext.* f:
+copy journals.* f:
+copy coll.* f:
+copy country.* f:
+copy ctry-rep.* f:
+copy reliable.* f:
+copy status.* f:
+copy par-rep.* f:
+copy distable.* f:
+copy counrep2.* f: 
+copy hostref2.* f:
+copy famref1.* f:
+copy famrep2.* f:
+copy keyrep.* f:
+copy key-rep.* f:
+copy authrep.* f:
+copy par-rep1.* f:
+copy hfamrep2.* f:
+copy famrep1.* f:
+copy Prfmctry.* f:
+copy famrep.* f:
+copy lschnoref.* f:
+copy hfamrep.* f:
+copy hostref.* f:
+copy hstnoref.* f:
+copy hfamrep3.* f:
+copy lstchrep.* f:
+copy chdstref.* f:
+copy hstnorf1.* f:
+copy hostref1.* f:
+copy famnames.* f:
+copy taxnoref.* f:
+copy lstfgref.* f:
+copy reftable.* f:
+copy relation.* f:
+copy taxrep01.* f:
+copy Stat-rep.* f:
+copy key-rep.* f:
+copy ctry-rep.* f:
+copy fam-rep.* f:
+copy keywords.* f:
+copy key-rep.* f:
+copy h-fam.* f:
+copy p-type.* f:
+copy fgnames.* f:
+copy Aamain.sc f:
+copy distrib.sc f:
+copy taxon.sc f:
+copy hosfind1.sc f:
+copy hosfind2.sc f:
+copy authlook.sc f:
+copy hosrep1.sc f:
+copy hosrep2.sc f:
+copy reflook.sc f:
+copy sc-cat.sc f:
+copy init.sc f:
+copy protrec.sc f:
+copy valname.* f:
+f:
+cd\
+f:
+logout
